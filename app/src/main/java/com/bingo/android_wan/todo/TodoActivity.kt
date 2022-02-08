dba6dd3d359ab20baa9be0f955a030e65f4509fe
renbin <renbin@chebada.com>
2022-02-08 14:14:30 +0800

    完善页面

--- app/src/main/java/com/bingo/android_wan/todo/TodoActivity.kt
+++ app/src/main/java/com/bingo/android_wan/todo/TodoActivity.kt
@@ -1,20 +1,36 @@
 package com.bingo.android_wan.todo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.bingo.android_wan.R
+import com.bingo.android_wan.databinding.ActivityTodoBinding
+import com.bingo.android_wan.multipleflutters.SingleFlutterActivity
 import com.bingo.android_wan.route.TodoRoute
 import com.bingo.router.annotations.Route
 
 @Route(pathClass = TodoRoute.Main::class)
 class TodoActivity : AppCompatActivity() {
+    val binding: ActivityTodoBinding by lazy {
+        return@lazy DataBindingUtil.setContentView(this, R.layout.activity_todo)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding.apply {
+
+        }
 
 
+    }
 
+    fun add(view: View) {
+        val intent = Intent(this, SingleFlutterActivity::class.java)
+        intent.putExtra("functionName", "main")
+        intent.putExtra("route", "/todo/list")
+        startActivity(intent)
 
     }
 }
